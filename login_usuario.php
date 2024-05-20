c3db158bd18c8b2a156f5aa17e70b488438b493e
victor <victorfire99@gmail.com>
2024-05-20 19:18:34 -0300

    problema login resolvido

--- login_usuario.php
+++ login_usuario.php
@@ -1,26 +1,21 @@
 <?php 
-
 require_once('../Projeto-Integrador-BrazilStore/backend/classes/usuarios/cliente.php');
 require_once('../Projeto-Integrador-BrazilStore/backend/database/DAO/ClienteDAO.php');
 
-if (isset($_POST['submit'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
     $clienteDAO = new ClienteDAO();
 
     if ($clienteDAO->autenticar($email, $senha)){
-
-        echo "Login feito com sucessos!";
+        echo "Login feito com sucesso!";
         exit(); 
     } else {
-        header('location: login.php');
-        
+        header('Location: login.php');
+        exit(); 
     }
+} else {
+    echo "Requisição inválida.";
 }
-
-
-
-
-
 ?>
