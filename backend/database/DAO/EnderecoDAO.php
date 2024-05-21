86353f990b6c19564375a6d24775770935a806c6
Caio Tomaz <126210948+caiotomaza@users.noreply.github.com>
2024-05-21 12:57:17 -0300

    Update EnderecoDAO.php

--- backend/database/DAO/EnderecoDAO.php
+++ backend/database/DAO/EnderecoDAO.php
@@ -7,16 +7,22 @@ require_once(__DIR__ . '/../../classes/usuarios/endereco.php');
 class EnderecoDAO{
     
     public function create(Endereco $endereco){
-        $sql = 'INSERT INTO endereco.endereco (logradouro, numero, bairro,	cep, id_cidade) values (?,?,?,?,?)';
+        $sql = 'INSERT INTO endereco.endereco (logradouro, numero, bairro,	cep) values (?,?,?,?)';
         $stmt = Conexao::getConn()->prepare($sql);
         $stmt->bindValue(1, $endereco->getLogradouro());
         $stmt->bindValue(2, $endereco->getNumero());
         $stmt->bindValue(3, $endereco->getBairro());
         $stmt->bindValue(4, $endereco->getBairro());
         $stmt->bindValue(5, $endereco->getCep());
-        $stmt->bindValue(6, $endereco->getId_cidade());
         $stmt->execute();
-    } // Tem que ver como o front vai coletar as informações do usuario (Trello).
+    }
+
+    public function create_mun(Endereco $endereco){
+        $sql = 'INSERT INTO endereco.cidade (nome) values (?,?,?,?)';
+        $stmt = Conexao::getConn()->prepare($sql);
+        $stmt->bindValue(1, $endereco->getLogradouro());
+        $stmt->execute();
+    }
 
     public function read($Endereco){
         $sql = "SELECT endereco.endereco.*, usuario.cliente.id_endereco
