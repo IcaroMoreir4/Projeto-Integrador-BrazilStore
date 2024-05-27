c32a7829ce27a5e10239f21f4d394c76f17cd1ee
victor <victorfire99@gmail.com>
2024-05-27 15:34:32 -0300

    produtos adicionados

--- login_usuario.php
+++ login_usuario.php
@@ -9,11 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['submit'])) {
     $clienteDAO = new ClienteDAO();
 
     if ($clienteDAO->autenticar($email, $senha)){
-<<<<<<< HEAD
         header('location: home.html');
-=======
-        header('location: molde-com-conta.php');
->>>>>>> 9a575cf5da9ab397bbcd54bcae205c070da183d2
         exit(); 
     } else {
         header('Location: index.html');
