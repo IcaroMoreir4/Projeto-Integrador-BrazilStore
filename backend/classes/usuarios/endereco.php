c16a39adfe892d13ec003b5ecc91ab840be163ba
Caio Tomaz <126210948+caiotomaza@users.noreply.github.com>
2024-05-28 12:43:24 -0300

    Correção das classes endereço

--- backend/classes/usuarios/endereco.php
+++ backend/classes/usuarios/endereco.php
@@ -6,25 +6,17 @@ class endereco{
     protected $numero;
     protected $bairro;
     protected $cep;
-    protected $id_cidade;
     protected $nome_cidade;
-    protected $cep_cidade;
-    protected $id_estado;
     protected $nome_estado;
-    protected $sigla;
 
-    public function __construct($id, $logradouro, $numero, $bairro, $cep, $id_cidade, $nome_cidade, $cep_cidade, $id_estado, $nome_estado, $sigla){
+    public function __construct($id, $logradouro, $numero, $bairro, $cep, $nome_cidade, $nome_estado){
         $this-> id = $id;
         $this-> logradouro = $logradouro;
         $this-> numero = $numero;
         $this-> bairro = $bairro;
         $this-> cep = $cep;
-        $this-> id_cidade = $id_cidade;
         $this-> nome_cidade = $nome_cidade;
-        $this-> cep_cidade = $cep_cidade;
-        $this-> id_estado = $id_estado;
         $this-> nome_estado = $nome_estado;
-        $this-> sigla = $sigla;
     }
 
 
@@ -49,28 +41,12 @@ class endereco{
         return $this->cep;
     }
 
-    public function getId_cidade(){
-        return $this->id_cidade;
-    }
-
     public function getNome_cidade(){
         return $this->nome_cidade;
     }
 
-    public function getCep_cidade (){
-        return $this->cep_cidade;
-    }
-
-    public function getId_estado(){
-        return $this->id_estado;
-    }
-
     public function getNome_estado(){
         return $this->nome_estado;
-    }
-
-    public function getSigla(){
-        return $this->sigla;
     }
 
 
@@ -95,27 +71,12 @@ class endereco{
         $this-> cep = $cep;
     }
 
-    public function setId_cidade($id_cidade){
-        $this-> id_cidade = $id_cidade;
-    }
-
     public function setNome_cidade($nome_cidade){
         $this-> nome_cidade = $nome_cidade;
     }
 
-    public function setCep_cidade($cep_cidade){
-        $this-> cep_cidade = $cep_cidade;
-    }
-
-    public function setId_estado($id_estado){
-        $this-> id_estado = $id_estado;
-    }
 
     public function setNome_estado($nome_estado){
         $this-> nome_estado = $nome_estado;
-    }
-
-    public function setSigla($sigla){
-        $this-> sigla = $sigla;
     }
 }
