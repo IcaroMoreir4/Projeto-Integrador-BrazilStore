d12f84712dbb75d777a5257b3821aea551963e2a
Caio Tomaz <126210948+caiotomaza@users.noreply.github.com>
2024-05-30 16:16:25 -0300

    Criação da pasta implementação e add do endereco

---
+++ backend/implementacao/imp_endereco/conslt_ende.php
@@ -0,0 +1,22 @@
+<?php
+    require_once('../backend/classes/usuarios/endereco.php');
+    require_once('../backend/database/DAO/EnderecoDAO.php');
+
+    session_start();
+    //$_SESSION['user_id'] = 1; // Teste
+
+    //Proteção
+    if (!isset($_SESSION['user_id'])) {
+        //echo 'Insira um id de usuario!'; // Teste
+        header('Location: index.php');
+        exit();
+    }
+
+    $dao = new EnderecoDAO;
+
+    //Função para consultar os endereços cadastrados.
+    if (isset($_GET['consl_ende'])) {
+        $consl_ende = $dao->read($_SESSION['user_id']);
+        $_SESSION['end_id'] = $consl_ende ['id'];
+    }
+?>
