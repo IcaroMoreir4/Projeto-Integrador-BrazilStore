7c44e936766c28f1e62ff33ecbfa9f37fe5284bb
victor <victorfire99@gmail.com>
2024-05-30 17:39:10 -0300

    altt

--- login_usuario.php
+++ login_usuario.php
@@ -1,6 +1,6 @@
 <?php 
-require_once('../../classes/usuarios/cliente.php');
-require_once('../../database/DAO/ClienteDAO.php');
+require_once('../Projeto-Integrador-BrazilStore/backend/classes/usuarios/cliente.php');
+require_once('../Projeto-Integrador-BrazilStore/backend/database/DAO/ClienteDAO.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
     $email = $_POST['email'];
