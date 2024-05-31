7ca94bc980e5a98a57e51ab8c802948a85df1a86
Rodrigoflima1903 <73724636+Rodrigoflima1903@users.noreply.github.com>
2024-05-31 00:35:14 -0300

    AdminExcluirProduto

--- test/historicos_pedidos.php
+++ test/historicos_pedidos.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listar Pedidos</title>
+    <title>Listar pedidos</title>
 </head>
 <body>
 
