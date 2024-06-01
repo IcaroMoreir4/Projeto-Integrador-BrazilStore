4d3db42eb53ab2a54d11e01c6ae285ebc3cecf47
Rodrigoflima1903 <73724636+Rodrigoflima1903@users.noreply.github.com>
2024-06-01 11:23:07 -0300

    Fazer_pedido

--- test/fazer_pedido.php
+++ test/fazer_pedido.php
@@ -1,32 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrinho de Compras</title>
+</head>
+<body>
+    <h2>Carrinho de Compras</h2>
+    <div>
+        <?php
+        require_once('../backend/database/DAO/CarrinhoDAO.php');
+        session_start();
 
-require_once('../backend/database/DAO/CarrinhoDAO.php');
+        // Inicializar a variável $total
+        $total = 0;
 
-session_start();
+        if (isset($_SESSION['id_item'])) {
+            $id_item = $_SESSION['id_item'];
+            $carrinhoDAO = new CarrinhoDAO();
+            $itens_carrinho = $carrinhoDAO->read($id_item);
 
-// Inicializar a variável $total
-$total = 0;
+            if ($itens_carrinho) {
+                foreach ($itens_carrinho as $item) {
+                    $total += $item['valor'];
+                }
+                echo 'Total do carrinho: ' . $total;
 
-if (isset($_SESSION['id_item'])) {
-    $id_item = $_SESSION['id_item'];
-    $carrinhoDAO = new CarrinhoDAO();
-    $itens_carrinho = $carrinhoDAO->read($id_item);
+                $_SESSION['comprar']->create_pedido($total);
 
-    if ($itens_carrinho){
-        foreach ($itens_carrinho as $item) {
-            $total += $item['valor'];
-         }
-        echo 'Total do carrinho: ' . $total;
-
-
-        $_SESSION['comprar']->create_pedido($total);
-
-        header('Location: compra.php');
-            exit();
-
-    } else {
-        echo 'Nenhum item encontrado' . $valor;
-    }
-}
-
-?>
+                header('Location: compra.php');
+                exit();
+            } else {
+                echo 'Nenhum item encontrado';
+            }
+        } else {
+            echo 'Nenhum item encontrado';
+        }
+        ?>
+    </div>
+</body>
+</html>
