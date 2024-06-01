a61839e8d0c69c3f71559d2a8699b2e7e866cbb8
Rodrigoflima1903 <73724636+Rodrigoflima1903@users.noreply.github.com>
2024-06-01 13:15:51 -0300

    fazer pedido

--- uploads/fazer_pedido.php
+++ uploads/fazer_pedido.php
@@ -2,26 +2,7 @@
 session_start();
 ?>
 
-<?php
 
-require_once('../Projeto-Integrador-BrazilStore/backend/classes/comercio/carrinho_item.php');
-require_once('../Projeto-Integrador-BrazilStore/backend/database/DAO/CarrinhoDAO.php');
-
-$conexao_path = __DIR__ . '/../conexao.php';
-$carrinho_item_path = __DIR__ . '/../../classes/comercio/carrinho_item.php';
-
-if (file_exists($conexao_path)) {
-    require_once($conexao_path);
-} else {
-    die("O arquivo de conexão não foi encontrado em $conexao_path");
-}
-
-if (file_exists($carrinho_item_path)) {
-    require_once($carrinho_item_path);
-} else {
-    die("O arquivo do carrinho de itens não foi encontrado em $carrinho_item_path");
-}
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -39,22 +20,21 @@ if (file_exists($carrinho_item_path)) {
         // Inicializar a variável $total
         $total = 0;
 
-        if (isset($_SESSION['id'])) {
-            $id_item = $_SESSION['id'];
+        if (isset($_SESSION['valor'])) {
+            $id_item = $_SESSION['valor'];
             $carrinhoDAO = new CarrinhoDAO();
             $itens_carrinho = $carrinhoDAO->read();
 
             if ($itens_carrinho) {
                 foreach ($itens_carrinho as $item) {
-                    // Aqui presumo que você queira acessar o valor do item
+                    
                     $total += $item['valor'];
                 }
                 echo 'Total do carrinho: ' . $total;
 
                 $_SESSION['comprar']->create_pedido($total);
 
-                // Removido o redirecionamento aqui
-
+                
             } else {
                 echo 'Nenhum item encontrado';
             }
